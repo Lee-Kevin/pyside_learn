5fa5dfe612a088ee2cad1a2ffba4b25ccc1cb6e5
Lee-Kevin <jiankai.li@seeed.cc>
2016-10-17 20:17:33 +0800

    add some file

--- Layout/GridLayout.py
+++ Layout/GridLayout.py
@@ -34,12 +34,10 @@ class Example(QtGui.QWidget):
         self.setWindowTitle('Calculator')
         self.show()
 
-
 def main():
     app = QtGui.QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     main()
